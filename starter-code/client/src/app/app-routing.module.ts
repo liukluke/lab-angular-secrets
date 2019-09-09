22f3f9e2a18f226f8784afdf5a7139e883ad63bd
liukluke <luca.manfredi1987@gmail.com>
2019-09-09 12:33:44 +0200

    fix service

--- starter-code/client/src/app/app-routing.module.ts
+++ starter-code/client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthLoginComponent } from './components/auth-login/auth-login.component';
+import { AuthSignupComponent } from './components/auth-signup/auth-signup.component';
+import { MyPrivatePageComponent } from './components/my-private-page/my-private-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: AuthSignupComponent },
+  { path: 'login', component: AuthLoginComponent },
+  { path: 'private', component: MyPrivatePageComponent },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- starter-code/client/src/app/services/session.service.ts
+++ starter-code/client/src/app/services/session.service.ts
@@ -1,7 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable()
 export class SessionService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  handleError(e: any): Observable<never> {
+    return throwError(e.message);
+  }
+
+  signup(user: any): Observable<HttpClient> {
+    return this.httpClient.post(`/signup`, user)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
+  login(user: any) {
+    return this.httpClient.post(`/login`, user)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
+  logout(): Observable<HttpClient> {
+    return this.httpClient.post(`/logout`, {})
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
+  isLoggedIn(): Observable<any> {
+    return this.httpClient.get(`/loggedin`)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
 }
